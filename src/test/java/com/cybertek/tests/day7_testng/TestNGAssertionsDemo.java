132975638c0f2ce7aa566a6bead171bef40e1a85
CybertekSchool <github@cybertekschool.com>
2020-09-03 13:38:48 -0400

    Day7: RadioButtonTest added

--- src/test/java/com/cybertek/tests/day7_testng/TestNGAssertionsDemo.java
+++ src/test/java/com/cybertek/tests/day7_testng/TestNGAssertionsDemo.java
@@ -27,8 +27,39 @@ public class TestNGAssertionsDemo {
 
     @Test
     public void test2(){
-        Assert.assertEquals("test2","test2");
+        Assert.assertEquals("test2","test22","verify test2");
     }
+
+    @Test
+    public void test3(){
+        String expectedTitle ="Cyb";
+        String actualTitle = "Cybertek";
+
+        Assert.assertTrue(actualTitle.startsWith(expectedTitle),"Verify title starts with Cyb");
+
+    }
+
+    @Test
+    public void test4(){
+        //verify email contains @ sign
+        String email = "mikesmith.com";
+
+        Assert.assertTrue(email.contains("@"),"verify email contains @");
+
+    }
+
+    @Test
+    public void test5(){
+        Assert.assertFalse(0>1,"verify that 0 is not greater than 1");
+
+    }
+
+    @Test
+    public void test6(){
+        Assert.assertNotEquals("one","two");
+    }
+
+
 
     @AfterMethod
     public void tearDown(){
