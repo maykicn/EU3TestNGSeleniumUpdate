cd70bb0a61ca2d97fd481f12d1993d095ee99487
CybertekSchool <github@cybertekschool.com>
2020-09-17 12:59:38 -0400

    Day11: WebTables Class prepared

---
+++ src/test/java/com/cybertek/tests/day11_webtables/WebTablesExample.java
@@ -0,0 +1,41 @@
+package com.cybertek.tests.day11_webtables;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class WebTablesExample {
+
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get("http://practice.cybertekschool.com/tables");
+    }
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.quit();
+    }
+
+    @Test
+    public void test1(){
+
+
+    }
+
+    @Test
+    public void test2(){
+
+
+    }
+}
