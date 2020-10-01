33feb2327c27d5cbe7e22d02fc6c181deba0b504
CybertekSchool <github@cybertekschool.com>
2020-10-01 13:25:43 -0400

    Day14: ExtentDemoTest Completed

--- src/test/java/com/cybertek/tests/day14_extent_reports/ExtentDemoTest.java
+++ src/test/java/com/cybertek/tests/day14_extent_reports/ExtentDemoTest.java
@@ -4,7 +4,9 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.cybertek.utilities.ConfigurationReader;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class ExtentDemoTest {
 
@@ -37,6 +39,35 @@ public class ExtentDemoTest {
           report.setSystemInfo("Environment","QA");
           report.setSystemInfo("Browser", ConfigurationReader.get("browser"));
           report.setSystemInfo("OS",System.getProperty("os.name"));
+
+      }
+
+      @Test
+      public void test1(){
+          //give name to current test
+          extentLogger= report.createTest("TC123 Login as Driver Test");
+
+         //test steps
+          extentLogger.info("Open Chrome Browser");
+
+          extentLogger.info("Go to this URL");
+
+          extentLogger.info("Enter driver username password");
+
+          extentLogger.info("Click Login");
+
+          extentLogger.info("Verify logged in");
+
+          //pass()--> marks the test case as passed
+          extentLogger.pass("TC123 is passed");
+
+      }
+
+
+      @AfterMethod
+        public void teardown(){
+          //this is when the report is actually created
+          report.flush();
 
       }
 
